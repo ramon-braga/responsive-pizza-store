c34a9bc8d543322db730f38b055791b2be83e34e
ramon-braga <ramonvbraga@gmail.com>
2023-10-31 15:29:28 -0300

    listed all items info on the index page

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,10 @@ const docAll = el => document.querySelectorAll(el); // shorthand
 pizzaJson.map((item, index) => {
     let pizzaItem = doc('.models .pizza-item').cloneNode(true);
 
-    doc('.pizza-area').append(pizzaItem);
+    pizzaItem.querySelector('.pizza-item--img img').setAttribute('src', item.img);
+    pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`;
+    pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
+    pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
 
+    doc('.pizza-area').append(pizzaItem);
 });
