31419476d294d207981fe69887875ad5e7e279cb
ramon-braga <ramonvbraga@gmail.com>
2023-10-31 16:47:37 -0300

    adding animation window using js

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,6 +9,16 @@ pizzaJson.map((item, index) => {
     pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`;
     pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
     pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
+    pizzaItem.querySelector('a').addEventListener('click', (e) => {
+        e.preventDefault();
+
+        doc('.pizzaWindowArea').style.opacity = 0;
+        doc('.pizzaWindowArea').style.display = 'flex';
+        setTimeout(() => {
+            doc('.pizzaWindowArea').style.opacity = 1;
+        }, 100);
+
+    });
 
     doc('.pizza-area').append(pizzaItem);
 });
