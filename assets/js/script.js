0e3a3a92f5e1e64feddad34b8b2593ad67fc4481
ramon-braga <ramonvbraga@gmail.com>
2023-10-31 22:16:14 -0300

    improved logic on adding items to the cart

--- assets/js/script.js
+++ assets/js/script.js
@@ -87,11 +87,20 @@ docAll('.pizzaInfo--size').forEach((size, sizeIndex) => {
 doc('.pizzaInfo--addButton').addEventListener('click', () => {
     let size =  parseInt(doc('.pizzaInfo--size.selected').getAttribute('data-key'));
 
-    cart.push({
-        id: pizzaJson[modalKey].id,
-        size,
-        qt: modalQt
-    });
+    let identifier = pizzaJson[modalKey].id + '@' + size;
+    let key = cart.findIndex(item => item.identifier == identifier);
+
+    if (key > -1) {
+        cart[key].qt += modalQt;
+
+    } else {
+        cart.push({
+            identifier,
+            id: pizzaJson[modalKey].id,
+            size,
+            qt: modalQt
+        });
+    }
 
     closeModal();
 });
